502cf4cb01d91340383d3c5106985f5949fe66ad
David Rogers <drogers@ucvts.tec.nj.us>
2019-10-14 09:20:38 -0400

    initial commit

--- src/ProblemSet4.java
+++ src/ProblemSet4.java
@@ -1,17 +1,17 @@
 /**
  * Problem Set 4.
- * 
+ *
  * It's time to put your skills to the test. This problem set focuses on using
  * iterative control structures. The `main` method is done for you. Lines 30-39
  * trigger each of the predefined methods, which you can think of as self-contained
  * executable pieces of logic. Write your code for each exercise in the
  * corresponding method.
- * 
+ *
  * The specifications for each exercise are outlined below. Your job is to write
  * lines of code to produce answers to my questions. Each exercise requires that
  * you ask the user to enter one or more values. Your code must meet the
  * requirements set forth in this section (as well as the *Deliverables* section).
- * 
+ *
  * Work through these exercises on your own. Experiment, make mistakes, ask
  * questions, and fix your mistakes. It's the only way to get good at programming.
  */
@@ -19,14 +19,14 @@
 import java.util.Scanner;
 
 public class ProblemSet4 {
-    
+
     private static Scanner in = new Scanner(System.in);
-    
+
     public static void main(String[] args) {
         ProblemSet4 ps = new ProblemSet4();
-        
+
         // comment out or uncomment as needed
-        
+
         ps.sum();
         ps.reverse();
         ps.digits();
@@ -37,119 +37,158 @@ public class ProblemSet4 {
         ps.mario();
         ps.luigi();
         ps.credit();
-                
+
         in.close();
     }
-    
+
     /*
      * Exercise 1.
-     * 
+     *
      * Prompt the user to enter two integers. The first integer is a lower bound, and
      * the second integer is an upper bound.
-     * 
+     *
      * Compute the sum of all even integers between the lower and upper bounds,
      * including the bounds themselves.
      */
-    
-    public void sum() {
 
+    public void sum() {
+      long sum = 0;
+       long lowerBound;
+       long upperBound;
+
+       do
+       {
+           System.out.print("\nLower bound: ");
+           lowerBound = in.nextLong();
+           System.out.print("Upper bound: ");
+           upperBound = in.nextLong();
+       } while (lowerBound > upperBound);
+
+       if ((lowerBound % 2) != 0) {
+           lowerBound = lowerBound+1;
+       }
+
+       for (long i = lowerBound; i <= upperBound; i = i + 2) {
+           sum += i;
+       }
+
+       System.out.printf("\n%d.\n", sum);
     }
-    
+
     /*
      * Exercise 2.
-     * 
+     *
      * Prompt the user to enter a positive integer. Print the digits of this integer
      * in reverse order.
      */
-    
+
     public void reverse() {
 
+      long userInteger = 0;
+      do
+      {
+          System.out.print("\nPositive integer: ");
+          userInteger = in.nextLong();
+      } while (userInteger < 1);
+
+
+
+//Set this whole thing up but in a for loop where it goes for the whole number
+      long ones = userInteger%10;
+      long tens = (userInteger/10)%10;
+      long hundreds = (userInteger/100)%10;
+      long thousands = (userInteger/1000)%10;
+      System.out.print(ones);
+      System.out.print(tens);
+      System.out.print(hundreds);
+      System.out.print(thousands);
+
     }
-    
+
     /*
      * Exercise 3.
-     * 
+     *
      * Prompt the user to enter a positive integer. Compute the sum of all of the odd
      * digits in the integer.
      */
-    
+
     public void digits() {
 
     }
-    
+
     /*
      * Exercise 4.
-     * 
+     *
      * Prompt the user to enter a series of non-negative integers. When the user
      * enters a negative integer, you can assume he or she is done entering values.
      * What is the average of the values entered?
      */
-    
+
     public void average() {
 
     }
-    
+
     /*
      * Exercise 5.
-     * 
+     *
      * Prompt the user to enter a non-negative integer. Is this number prime?
      */
-    
+
     public void prime() {
 
     }
-    
+
     /*
      * Exercise 6.
-     * 
+     *
      * Prompt the user to enter a positive integer in the range [1, 92]. If the
      * integer the user enters is called n, what is the nth Fibonacci number?
      */
-    
+
     public void fibonacci() {
 
     }
-    
+
     /*
      * Exercise 7.
-     * 
+     *
      * Prompt the user to enter a positive integer. What are its factors?
      */
-    
+
     public void factors() {
 
     }
-    
+
     /*
      * Exercise 8.
-     * 
+     *
      * Prompt the user to enter an integer between 1 and 24 (inclusive). Print a Super
      * Mario-style half-pyramid of the specified height.
      */
-    
-    public void mario() {        
+
+    public void mario() {
 
     }
-    
+
     /*
      * Exercise 9.
-     * 
+     *
      * Prompt the user to enter an odd integer between 1 and 24 (inclusive). Print a
      * Super Mario-style full pyramid of the specified height.
      */
-    
+
     public void luigi() {
 
     }
-    
+
     /*
      * Exercise 10.
-     * 
+     *
      * Prompt the user to enter a credit card number (not a real one!). According to
      * Luhn's algorithm, is the credit card number valid?
      */
-    
+
     public void credit() {
-        
+
     }
 }
